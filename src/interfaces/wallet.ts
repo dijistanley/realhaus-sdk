c7f863722180b7ea1a7796558cfa71b6046ab79d
Stanley Diji <dijistanley@gmail.com>
2023-01-25 09:40:12 -0700

    chore: add interface, enums for transaction management system (#20)
    * chore: add interface, enums for transaction management system
    
    * chore: increase sdk version
    
    Co-authored-by: Stanley Diji <stanley.diji@autodesk.com>

---
+++ src/interfaces/wallet.ts
@@ -0,0 +1,37 @@
+import { TransactionStatus, TransactionType, TrxLogType } from '../enums/wallet';
+import { IEntity } from './entity';
+
+export interface IWallet {
+  amount: number;
+  owner: string;
+  lastTransactionId?: string;
+}
+
+export type IDWallet = IEntity & IWallet;
+
+export interface ITransaction {
+  type: TransactionType; // CREDIT / DEBIT
+  amount: number;
+  balance: number;
+  timestamp: number;
+  notes: string;
+  trxLogId: string;
+}
+
+export type IDTransaction = IEntity & ITransaction;
+
+
+export interface ITransactionLog {
+  timestamp: number;
+  type: TrxLogType;
+  senderId: string;
+  amount: number;
+  receiverId: string;
+  status: TransactionStatus;
+  referenceId?: string;
+  naration: string;
+  notes?: string;
+  source: string;
+}
+
+export type IDTransactionLog = IEntity & ITransactionLog;
