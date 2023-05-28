712cee08754b26ab7d5a415f6e26edb19428cf11
Mbawuike Kelechi <kmbawuike@gmail.com>
2023-05-28 18:59:33 +0100

    added plaid interface (#36)

--- src/interfaces/bankAccount.ts
+++ src/interfaces/bankAccount.ts
@@ -1,11 +1,5 @@
 import { IEntity } from './entity';
-import { Vendors } from '../enums/bankAccount';
-
-export interface IFlinksContext {
-  loginId?: string;
-  institution?: string;
-  accountId?: string;
-}
+import { BankAccountPurpose, Vendors } from '../enums/bankAccount';
 
 export interface IPaypalContext {
   payer_id?: string;
@@ -21,15 +15,30 @@ export interface IPaypalToken {
   nonce: string;
 }
 
+export interface IPlaidContext {
+  accessToken?: string;
+  requestId?: string;
+  itemId?: string;
+  institution?: string;
+  institutionId?: string;
+}
+
 export interface IBankConnection {
   timestamp: number;
   vendor: Vendors;
-  context: IFlinksContext & IPaypalContext;
+  context: IPlaidContext & IPaypalContext;
 }
 
 export interface IBankAccount {
   payments: IBankConnection[];
   payouts: IBankConnection[];
+}
+
+export interface IExchangePlaidLinkTokenRequest {
+  publicToken: string;
+  institution?: string;
+  institutionId?: string;
+  bankAccountPurpose: BankAccountPurpose;
 }
 
 export type IdBankAccount = IEntity & IBankAccount;
