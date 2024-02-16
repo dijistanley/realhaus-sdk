c34515505ca9bf08c5d16db9613e26ce84eb9d69
Madu David <Davidmadu24@gmail.com>
2024-02-16 16:41:24 +0100

    Feat: david/coupon management system types

---
+++ src/enums/coupon.ts
@@ -0,0 +1,14 @@
+export enum CouponTitle {
+  First_Lease_Half_Off = "First_Lease_Half_Off",
+  REFERRAL = "REFERRAL",
+}
+
+export enum CouponType {
+  PERCENTAGE = "PERCENTAGE",
+  DOLLAR = "DOLLAR",
+}
+
+export enum CouponStatus {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+}
---
+++ src/interfaces/coupon.ts
@@ -0,0 +1,15 @@
+import { CouponStatus, CouponTitle, CouponType } from "../enums/coupon";
+
+export interface ICoupon {
+  title: CouponTitle;
+  type: CouponType;
+  value: number;
+  status: CouponStatus;
+}
+
+export interface IUserCoupon {
+  start: number;
+  end: number;
+  couponID: string;
+  usage: number;
+}
